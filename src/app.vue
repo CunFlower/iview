43c1386f68bae437872e140f3fe233fd4603d7c6
OFFICE\yueqin.zuo <yueqin.zuo@freemud.cn>
2018-07-03 20:07:57 +0800

    配置环境

--- src/app.vue
+++ src/app.vue
@@ -34,4 +34,7 @@ html,body{
     width: 100%;
     height: 100%;
 }
+.demo-spin-icon-load{
+    animation: ani-demo-spin 1s linear infinite;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import '@/locale';
 import 'iview/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
 import util from './libs/util';
-
+import axios from 'axios';
+import env from '../build/env';
 Vue.use(VueI18n);
 Vue.use(iView);
 
@@ -21,6 +22,45 @@ new Vue({
         currentPageName: ''
     },
     mounted () {
+        let $this=this
+        const ajaxUrl = env === 'development || test'
+            ? 'http://118.89.190.166:9980'
+            : env === 'production'
+                ? 'http://123.206.195.237:9980'
+                : 'http://118.89.190.166:9980';
+        console.log(ajaxUrl)
+
+        let instance = axios.create({
+            baseURL: ajaxUrl,
+            timeout: 30000
+        });
+        // 添加请求拦截器
+        instance.interceptors.request.use(function (config) {
+            $this.$Spin.show({
+                render: (h) => {
+                    return h('div', [
+                        h('Icon', {
+                            'class': 'demo-spin-icon-load',
+                            props: {
+                                type: 'load-c',
+                                size: 18
+                            }
+                        }),
+                        h('div', 'Loading')
+                    ])
+                }
+            });
+            return config;
+        }, function (error) {
+            return Promise.reject(error);
+        });
+        // 添加响应拦截器
+        instance.interceptors.response.use(function (response) {
+            $this.$Spin.hide();
+            return response;
+        }, function (error) {
+            return Promise.reject(error);
+        });
         this.currentPageName = this.$route.name;
         // 显示打开的页面的列表
         this.$store.commit('setOpenedList');
@@ -28,7 +68,10 @@ new Vue({
         // 权限菜单过滤相关
         this.$store.commit('updateMenulist');
         // iview-admin检查更新
-        util.checkUpdate(this);
+        //util.checkUpdate(this);
+        instance.get('login').then(res => {
+            
+        });
     },
     created () {
         let tagsList = [];
